9e690f88ecf6c9f6183297b7130e3d912210f000
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 15:52:36 +0530

    [pooja] Add: python program to remove element in array

---
+++ Arrays/4_removefirstocc.py
@@ -0,0 +1,15 @@
+
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to create and acces array
+
+'''
+
+arr = [10, 20, 30, 40, 50]
+el = 20
+arr.remove(el)
+print(arr)
