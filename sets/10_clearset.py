062acd8e5e96219257e92909b8ea0befb23b9bd4
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 00:16:54 +0530

    [pooja] Add: python program to clear set

---
+++ sets/10_clearset.py
@@ -0,0 +1,15 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to clear set
+
+'''
+
+my_set = {1, 2, 3, 4, 5}
+
+# Clear the set (remove all items)
+my_set.clear()
+print("After clearing:", my_set)
