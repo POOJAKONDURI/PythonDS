5f10f4759511b1000561fe1f98b29310c5507996
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 15:51:31 +0530

    [pooja] Add: python program to create and acces array

---
+++ Arrays/1_createarray.py
@@ -0,0 +1,15 @@
+
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to create and acces array
+
+'''
+#for memory efficiency import array
+arr = [10, 20, 30, 40, 50]
+print("Element at index 0:", arr[0])
+print("Element at index 1:", arr[1])
+print("Element at index 2:", arr[2])
