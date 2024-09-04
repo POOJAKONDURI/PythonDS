fc9def57d865caea197e3cf663125b297f228417
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 17:08:29 +0530

    [pooja] Add: python program to return uniques values of keys

---
+++ dictionary/7_uniquevalues.py
@@ -0,0 +1,16 @@
+
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to print unique values
+
+'''
+
+data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
+        {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
+unique_values = set(val for dic in data for val in dic.values())
+
+print("Unique Values:", unique_values)
