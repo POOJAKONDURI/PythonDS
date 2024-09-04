25c510e30841b45de07b652b076fd863b2e097d7
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 00:12:31 +0530

    [pooja] Add: python program to iterate over set

---
+++ sets/2_iterateoverset.py
@@ -0,0 +1,13 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to iterate over set
+
+'''
+
+my_set = {1,2,3,4,5}
+for item in my_set:
+    print(item)
