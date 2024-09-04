a279131df85a665b1a777547ed47e88017288328
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 15:51:48 +0530

    [pooja] Add: python program to reverse array

---
+++ Arrays/2_reversearray.py
@@ -0,0 +1,15 @@
+
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to create and acces array
+
+'''
+
+#reverses inside it only donot crete new array 
+arr = [10, 20, 30, 40, 50]
+arr.reverse()
+print(arr)
