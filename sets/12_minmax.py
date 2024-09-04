6593aa12ab8c653edf29b56e427a383ec014a74e
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 00:18:52 +0530

    [pooja] Add: python program to find min and max of set

---
+++ sets/12_minmax.py
@@ -0,0 +1,20 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to find max an dmin value of set 
+
+'''
+
+
+my_set = {1, 2, 3, 4, 5}
+
+max_value = max(my_set)
+min_value = min(my_set)
+
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
+
+
