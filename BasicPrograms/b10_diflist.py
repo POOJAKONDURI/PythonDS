c416d56986e4fb1d838e66d31d84e2d708369eed
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 15:00:28 +0530

    [pooja] Add: python program on difference of two listsy

---
+++ BasicPrograms/b10_diflist.py
@@ -0,0 +1,19 @@
+
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to display histogram of list
+
+'''
+# Define the color sets
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+
+# Find the difference
+unique_colors = color_list_1 - color_list_2
+
+# Print the result
+print(unique_colors)
