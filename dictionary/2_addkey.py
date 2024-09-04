5be078f96da280c9e1d23a23b6e58790384fe99e
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 17:05:38 +0530

    [pooja] Add: python program to add key to dictionaey

---
+++ dictionary/2_addkey.py
@@ -0,0 +1,17 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to add key
+
+'''
+
+
+d = {0: 10, 1: 20}
+
+
+d[2] = 30
+
+print("Updated Dictionary:", d)
