fcaa2756ec8e45f634d6e5c20c59172a11df4e25
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-04 14:57:37 +0530

    [pooja] Add: python program to show documnetation

---
+++ BasicPrograms/b4_pythondocumentation.py
@@ -0,0 +1,24 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to display color
+
+'''
+
+"""
+        description:
+            This program to display documnetation"
+        parameters:
+            none :
+        return:
+            Returns documentation of python
+    
+"""
+
+# Print the documentation of the abs() function
+print(abs.__doc__)
+
+
