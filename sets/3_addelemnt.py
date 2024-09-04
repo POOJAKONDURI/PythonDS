9b1469c6ff27f15b6c2f140d6a0f4352233e2369
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 00:13:02 +0530

    [pooja] Add: python program to add element to set

---
+++ sets/3_addelemnt.py
@@ -0,0 +1,14 @@
+'''
+
+    @Author:pooja
+    @Date:04-09-2024
+    @last modified by:pooja
+    @last modified time:04-09-2024
+    @title:program to form dict from string
+
+'''
+my_set = {1,2,3,4,5}
+my_set.add(6)
+#multiple numbers
+my_set.update([6,7])
+print(my_set)
