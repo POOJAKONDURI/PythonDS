47c97adf5413d234df3c8e48fd84b9c7c70143f8
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 16:24:54 +0530

    [pooja] Add: python program to find length od string

---
+++ strings/1_lenstring.py
@@ -0,0 +1,13 @@
+'''
+
+    @Author:pooja
+    @Date:05-09-2024
+    @last modified by:pooja
+    @last modified time:05-09-2024
+    @title:program to slice tuple
+
+'''
+
+my_string = "Hello, World!"
+length = len(my_string)
+print(length)  
