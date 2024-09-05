d87281dd38c6e728df2c30e72b8fba681caedfdb
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 16:26:56 +0530

    [pooja] Add: python program to convert to upper and lowercase

---
+++ strings/6_lowupcase.py
@@ -0,0 +1,32 @@
+
+
+'''
+
+    @Author:pooja
+    @Date:05-09-2024
+    @last modified by:pooja
+    @last modified time:05-09-2024
+    @title:program to replace first charecter with symbol
+
+'''
+
+
+def upper_lower_cases(s):
+      """
+        description:
+            This program gives 
+        parameters:
+            str : 
+        return:
+            Returns string with occurence of first character replaced with symbol
+    """
+
+      print("Uppercase:", s.upper())
+      print("Lowercase:", s.lower())
+
+
+def main():
+    upper_lower_cases("Hello, World!")
+
+if __name__ == "__main__":
+    main()
