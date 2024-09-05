aa70b915a1716036884edc54deaefc56eb3f7060
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 13:53:03 +0530

    [pooja] Add: python program to slice tuple

---
+++ tuple/9_slicetuple.py
@@ -0,0 +1,13 @@
+'''
+
+    @Author:pooja
+    @Date:05-09-2024
+    @last modified by:pooja
+    @last modified time:05-09-2024
+    @title:program to slice tuple
+
+'''
+
+my_tuple = (1, 2, 3, 4, 5)
+sliced_tuple = my_tuple[1:4]
+print(sliced_tuple) 
