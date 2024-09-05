56df755c6f23404a08bb0b1b0ca62032c7075da8
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 12:50:07 +0530

    [pooja] Add: python program to find sum of elements in a list

---
+++ lists/1_sum.py
@@ -0,0 +1,15 @@
+'''
+
+    @Author:pooja
+    @Date:05-09-2024
+    @last modified by:pooja
+    @last modified time:05-09-2024
+    @title:program to sum of list
+
+'''
+
+def sum_list(items):
+    return sum(items)
+
+# Example usage
+print(sum_list([1, 2, 3, 4]))  # Output: 10
