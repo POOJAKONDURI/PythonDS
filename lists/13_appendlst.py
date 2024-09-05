a2eb8c6b26fa65dd60202115222489abc68b73b8
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-05 13:00:25 +0530

    [pooja] Add: python program to append two lists

---
+++ lists/13_appendlst.py
@@ -0,0 +1,24 @@
+'''
+
+    @Author:pooja
+    @Date:05-09-2024
+    @last modified by:pooja
+    @last modified time:05-09-2024
+    @title:program to append two lsts
+
+'''
+
+
+
+def append_list(list1, list2):
+     """
+        description:
+            This program to append two lists"
+        parameters:
+            list1 and list2:
+        return:
+            Returns appended lst
+    """
+     return list1 + list2
+
+print(append_list([1, 2, 3], [4, 5, 6]))  
